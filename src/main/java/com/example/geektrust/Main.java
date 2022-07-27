67f2d2010c037a57643a2f1c6c030ce0827de9ec
Kumaran-PS <kumaran28oct@gmail.com>
2022-07-27 22:42:12 +0530

    made changes - input , main

--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -1,20 +1,44 @@
-package com.example.geektrust; 
+package com.example.geektrust;
 
-public class Main {
+import com.example.geektrust.Service.CalculatePoints;
+import com.example.geektrust.Utils.Constants;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main implements Constants {
     public static void main(String[] args) {
-        /*
-        Sample code to read from file passed as command line argument
+        int startDx = 0;
+        int startDy = 0;
+        int endDx = 0;
+        int endDy = 0;
+        String directionFacing = EMPTY_STRING;
         try {
-            // the file to be opened for reading
-            FileInputStream fis = new FileInputStream(args[0]);
-            Scanner sc = new Scanner(fis); // file to be scanned
-            // returns true if there is another line to read
+            FileInputStream inputFile = new FileInputStream(INPUT_FILE_PATH);
+            Scanner sc = new Scanner(inputFile);
             while (sc.hasNextLine()) {
-               //Add your code here to process input commands
+                String inputLine = sc.nextLine();
+                String[] inputData = inputLine.split(" ");
+                switch (inputData[INPUT_TYPE]) {
+                    case "SOURCE":
+                        startDx = Integer.parseInt(inputData[SOURCE_INDEX_X]);
+                        startDy = Integer.parseInt(inputData[SOURCE_INDEX_Y]);
+                        directionFacing = inputData[DIRECTION_FACING_INDEX];
+                        break;
+                    case "DESTINATION":
+                        endDx = Integer.parseInt(inputData[DESTINATION_INDEX_X]);
+                        endDy = Integer.parseInt(inputData[DESTINATION_INDEX_Y]);
+                        break;
+                    case "PRINT_POWER":
+                        CalculatePoints calculatePoints = new CalculatePoints();
+                        int powerLeft = calculatePoints.calculateRemainingPower(startDx, startDy, endDx, endDy, directionFacing);
+                        System.out.println("POWER " + powerLeft);
+                        break;
+                }
             }
-            sc.close(); // closes the scanner
+            sc.close();
         } catch (IOException e) {
+            System.out.println(e.getLocalizedMessage());
         }
-        */
     }
 }
--- src/test/java/com/example/geektrust/MainTest.java
+++ src/test/java/com/example/geektrust/MainTest.java
@@ -1,4 +1,14 @@
 package com.example.geektrust;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 public class MainTest {
+
+    @Test
+    public void testMain() throws IOException {
+        System.out.println("main");
+        Main.main(null);
+    }
 }
