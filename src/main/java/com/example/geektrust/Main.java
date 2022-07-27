565bac044e9b5d1be6232e764e88e35f6f1ac0bd
Kumaran-PS <kumaran28oct@gmail.com>
2022-07-27 23:01:18 +0530

    changed all input to input1.txt and " " as constant

--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -8,30 +8,30 @@ import java.util.Scanner;
 
 public class Main implements Constants {
     public static void main(String[] args) {
-        int startDx = 0;
-        int startDy = 0;
-        int endDx = 0;
-        int endDy = 0;
+        int coordinateX1 = 0;
+        int coordinateY1 = 0;
+        int coordinateX2 = 0;
+        int coordinateY2 = 0;
         String directionFacing = EMPTY_STRING;
         try {
             FileInputStream inputFile = new FileInputStream(INPUT_FILE_PATH);
             Scanner sc = new Scanner(inputFile);
             while (sc.hasNextLine()) {
                 String inputLine = sc.nextLine();
-                String[] inputData = inputLine.split(" ");
+                String[] inputData = inputLine.split(SPACE);
                 switch (inputData[INPUT_TYPE]) {
                     case "SOURCE":
-                        startDx = Integer.parseInt(inputData[SOURCE_INDEX_X]);
-                        startDy = Integer.parseInt(inputData[SOURCE_INDEX_Y]);
+                        coordinateX1 = Integer.parseInt(inputData[SOURCE_INDEX_X]);
+                        coordinateY1 = Integer.parseInt(inputData[SOURCE_INDEX_Y]);
                         directionFacing = inputData[DIRECTION_FACING_INDEX];
                         break;
                     case "DESTINATION":
-                        endDx = Integer.parseInt(inputData[DESTINATION_INDEX_X]);
-                        endDy = Integer.parseInt(inputData[DESTINATION_INDEX_Y]);
+                        coordinateX2 = Integer.parseInt(inputData[DESTINATION_INDEX_X]);
+                        coordinateY2 = Integer.parseInt(inputData[DESTINATION_INDEX_Y]);
                         break;
                     case "PRINT_POWER":
                         CalculatePoints calculatePoints = new CalculatePoints();
-                        int powerLeft = calculatePoints.calculateRemainingPower(startDx, startDy, endDx, endDy, directionFacing);
+                        int powerLeft = calculatePoints.calculateRemainingPower(coordinateX1, coordinateY1, coordinateX2, coordinateY2, directionFacing);
                         System.out.println("POWER " + powerLeft);
                         break;
                 }
